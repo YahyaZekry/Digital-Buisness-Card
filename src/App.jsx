a1e18d0c432a240a3023a11aad95e95f365ecba1
YahyaZekry <yahyazekry@gmail.com>
2022-09-01 04:47:36 +0200

    'hosting'

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import Info from "./components/Info";
 import About from "./components/About";
 import Interests from "./components/Interests";
 import Footer from "./components/Footer";
+import containerCSS from "./components/App.module.css";
 
 export default function App() {
   return (
-    <div className="container">
+    <div className={containerCSS["container--wrapper"]}>
       <Info />
       <About />
       <Interests />
